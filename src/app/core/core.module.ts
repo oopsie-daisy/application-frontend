d3b54c56a103bffa726106b302b0210e7ec9f875
gintare.balsyte <gintare.balsyte@insoft.lt>
2022-04-16 15:38:37 +0300

    updated project structure

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RouterModule} from '@angular/router';
+import {SharedModule} from 'src/app/shared/shared.module';
+
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    RouterModule
-  ],
-  exports: [
-    RouterModule
+    SharedModule
   ]
 })
-export class SharedModule { }
+export class CoreModule { }
